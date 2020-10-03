d87cd9a0c96edefb2569551dcfeb36e658326234
Abdullah <nogame722737@gmail.com>
2020-10-03 16:52:53 +0500

    Built Sticky nav

---
+++ resources/javascript/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $(".js--section-features").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $("nav").addClass("sticky");
+      } else {
+        $("nav").removeClass("sticky");
+      }
+    },
+    {
+      offset: "70px",
+    }
+  );
+});
