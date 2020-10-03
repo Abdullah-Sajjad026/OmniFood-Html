89c3aaf9e89c5dc0f24b80ca32d89d2cfb8bc939
Abdullah <nogame722737@gmail.com>
2020-10-03 19:24:36 +0500

    Animations added successfully

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+  // For Sticky Navigation
   $(".js--section-features").waypoint(
     function (direction) {
       if (direction == "down") {
@@ -11,4 +13,56 @@ $(document).ready(function () {
       offset: "70px",
     }
   );
+
+  // For Scrolling buttons
+
+    $('.js--scroll-to-plans').click(function(){
+      $('html, body').animate({scrollTop: $('.js--section-plans').offset().top} , 1000);
+    });
+    $('.js--scroll-to-start').click(function(){
+      $('html, body').animate({scrollTop: $('.js--section-features').offset().top} , 500);
+    });
+
+    // Smooth Scrolling
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+
+    // For Animations
+    $('.js--wp-1').waypoint(function(direction) {
+      $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+    }, {
+      offset: '50%'
+    });
+
+    $('.js--wp-2').waypoint(function(direction) {
+      $('.js--wp-2').addClass('animate__animated animate__backInUp');
+    }, {
+      offset: '50%'
+    });
+
+    $('.js--wp-3').waypoint(function(direction) {
+      $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+    }, {
+      offset: '50%'
+    });
+
+    $('.js--wp-4').waypoint(function(direction) {
+      $('.js--wp-4').addClass('animate__animated animate__pulse');
+    }, {
+      offset: '50%'
+    });
+
+    
 });
